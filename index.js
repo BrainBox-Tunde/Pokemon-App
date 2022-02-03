5cc709cfb23856941473692e4b1e37013414fabd
Tunde Ojebisi <realtundeonline@gmail.com>
2022-02-03 10:54:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+//Open your console and add the 1st code to change image
+
+
+//Open your console and add the 2nd code to change alt text
+
+// Then change the tree's image, shape and align text to center
+
